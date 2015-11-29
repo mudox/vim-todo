957531b7058c30acf8a3b7046f51a16b55af03a2
mudox <mudoxisme@gmail.com>
2015-11-30 05:42:34 +0800

    test.sh: remove shopt -s globstar lines

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,9 @@
 # recreat 'test-todo' tmux window
 tmux kill-window -t :=test-todo &>/dev/null
 tmux new-window -da -c ~/Git/vim-config/plugged/todo -n test-todo bash
-# enable bash '**' globbing
-tmux send-keys -t :=test-todo                                                 \
-  'shopt -s globstar'                                                         \
-  c-m
+
 # launch nvim & start Vader
-tmux send-keys -t :=test-todo                                                 \
-  "MDX_CHAMELEON_MODE=test-todo nvim ~/Git/neovim/**/*.vim -c 'Vader test/*'" \
+tmux send-keys -t :=test-todo                           \
+  "MDX_CHAMELEON_MODE=test-todo nvim -c 'Vader test/*'" \
   c-m
 tmux select-window -t :=test-todo
