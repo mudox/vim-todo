2658ebfe038eb41d3f39cf48974fe0f772ca0dba
mudox <mudoxisme@gmail.com>
2015-12-04 04:11:48 +0800

    split test case into 2 files, often run main.vader

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ tmux kill-window -t :=test-todo &>/dev/null
 tmux new-window -da -c ~/Git/vim-config/plugged/todo -n test-todo bash
 
 # launch nvim & start Vader
-tmux send-keys -t :=test-todo                           \
-  "MDX_CHAMELEON_MODE=test-todo nvim -c 'Vader test/*'" \
+tmux send-keys -t :=test-todo                                    \
+  "MDX_CHAMELEON_MODE=test-todo nvim -c 'Vader test/main.vader'" \
   c-m
 tmux select-window -t :=test-todo
